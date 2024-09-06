b46a2a445f3affd8186ee91ce7433cb7908cf681
Chuck Norris <me@example.com>
2024-09-06 12:45:31 +0200

    Perlin1D should work

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include "perlin.h"
 
 // Override base class with your custom functionality
 class Window : public olc::PixelGameEngine
 {
+	Perlin1D perlin;
 public:
 	Window()
 	{
@@ -16,12 +18,20 @@ public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+		perlin = Perlin1D(ScreenWidth());
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		Clear(olc::BLACK);
 		// Called once per frame, draws random coloured pixels
+		for (int i = 0; i < ScreenWidth(); i++) {
+			float val = perlin.get(i);
+			for (int j = val * ScreenHeight() / 2; j < ScreenHeight() / 2; j++) {
+				Draw(i, j, olc::GREEN);
+			}
+		}
 		return true;
 	}
 };
--- perlin.h
+++ perlin.h
@@ -11,30 +11,43 @@ class Perlin1D {
 public:
 	Perlin1D(int size=256, int octaves=8, float basedrop=2) : size(size), octaves(octaves), basedrop(basedrop) {
 		values = new float[size];
-		std::fill(values, values + size, 0);
+		seed = new float[size];
+		randomize_seed();
 	}
 
-	void reset_seed() {
+	~Perlin1D() {
+		delete[] values;
 		delete[] seed;
+	}
 
-		seed = new float[size];
+	void randomize_seed() {
 		for (int i = 0; i < size; i++) {
 			seed[i] = rand() / (float)RAND_MAX;
 		}
 	}
 
 	void calculate() {
+		std::fill(values, values + size, 0);
+
 		for (int x = 0; x < size; x++) {
+			float scale_factor = 1;
+			float scale_sum = 0;
 			for (int octave = 0; octave < octaves; octave++) {
 				int pitch = size >> octave;
 				int sample1 = x - x%pitch;
 				int sample2 = (sample1 + pitch) % size;
+				float blend = (x - sample1) / (float)pitch;
+				float value = ((1 - blend) * seed[sample1] + blend * seed[sample2]) * scale_factor;
+				values[x] += value;
+				scale_sum += scale_factor;
+				scale_factor /= basedrop;
 			}
+			values[x] /= scale_sum;
 		}
 	}
 
-	float get(float x) {
-		return 0;
+	float get(int x) {
+		return values[x];
 	}
 
 };
